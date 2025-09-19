e700d33445e0cbf4064bcf0006aad37f60778ccc
Diego-Dev26 <aio0032953@est.univalle.edu>
2025-09-19 06:50:55 -0400

    From React

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,11 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
+
 
 export const unstable_settings = {
   anchor: '(tabs)',
