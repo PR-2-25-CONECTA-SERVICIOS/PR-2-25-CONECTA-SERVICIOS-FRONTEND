ec35113761e91a0fbd1d170d80b9c7c312426be4
Diego-Dev26 <aio0032953@est.univalle.edu>
2025-11-19 08:12:16 -0400

    MapaWeb(Falta falfa hacer)

---
+++ metro.config.js
@@ -0,0 +1,13 @@
+const { getDefaultConfig } = require('@expo/metro-config');
+
+const config = getDefaultConfig(__dirname);
+
+config.resolver.assetExts.push(
+  'png',
+  'jpg',
+  'jpeg',
+  'gif',
+  'svg'
+);
+
+module.exports = config;
