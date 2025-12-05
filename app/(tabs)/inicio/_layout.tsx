34860b40efd8e592b56c44382ef629aff2a3bbb5
Jesus Espejo <133383873+Huicho06@users.noreply.github.com>
2025-12-04 22:32:18 -0400

    layout funcional

--- app/(tabs)/inicio/_layout.tsx
+++ app/(tabs)/inicio/_layout.tsx
@@ -1,1 +1,9 @@
+import { Stack } from "expo-router";
 
+export default function InicioLayout() {
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      {/* si no declaras screens manualmente, Expo los detecta automáticamente */}
+    </Stack>
+  );
+}
